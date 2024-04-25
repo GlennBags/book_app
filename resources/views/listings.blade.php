e39d3359977f41fae7ec22ed9331b4652c346adf
glennbagrowski <glenn.bagrowski@ministrybrands.com>
2024-04-25 06:01:37 -0400

    adding partials views; using hero

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,8 @@
 @extends('layout')
 
 @section('content')
+@include('partials._hero')
+
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @unless(empty($listings))
@@ -9,15 +11,13 @@
 @foreach($listings as $listing)
     <!-- Item 1 -->
     <div class="bg-gray-50 border border-gray-200 rounded p-6">
-        <div class="flex">
-            <img
-                    class="hidden w-48 mr-6 md:block"
-                    src="{{ $listing->imageLinks_thumbnail }}"
-                    alt=""
+        <div class="flex items-start">
+            <img class="hidden w-48 h-auto mr-6 md:block object-contain"
+                 src="{{ $listing->imageLinks_thumbnail }}"
             />
             <div>
                 <h3 class="text-2xl">
-                    <a href="show.html">{{$listing->title}}</a>
+                    <span>{{$listing->title}}</span>
                 </h3>
                 <div class="text-xl font-bold mb-4">{{ $listing->authors }}</div>
                 <div class="text-xl {{ $listing->subtitle ? 'font-bold' : '' }} mb-4">
